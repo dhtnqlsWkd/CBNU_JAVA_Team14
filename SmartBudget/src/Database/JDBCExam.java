329c68564a438994d14790c2acb8686bc7a30aea
CBNU <CBNU@CBNU-PC>
2017-12-01 16:29:56 +0900

    DB연동부 추가2

--- SmartBudget/src/Database/JDBCExam.java
+++ SmartBudget/src/Database/JDBCExam.java
@@ -1,4 +1,5 @@
 package Database;
+
 import java.sql.*;
 import java.util.*;
 
@@ -11,6 +12,7 @@ public class JDBCExam {
 	String url = "jdbc:mysql://localhost:3306/budget";
 	String user = "paki1019";
 	String password = "wlsgh123";
+	String tableName = "budget";
 
 	public JDBCExam() {
 		try {
@@ -36,30 +38,51 @@ public class JDBCExam {
 			System.out.println("[닫기 오류]\n" + e.getStackTrace());
 		}
 	}
-
-	public int productInsert(String p_date, int p_price, int p_in_out, int p_card) {
-		int resultValue = 0;
-
+	
+	
+	public void productInsert(String p_date, int p_price, int p_in_out, int p_card) {//추가
 		try {
-			String queryString = "INSERT INTO budget (`data`, `price`, `in_out`, `card`) VALUES"
-					+ " ('" + p_date + "', " + p_price + "', '" + p_in_out + "', '"+p_card + "')";
+			String queryString = "INSERT INTO " + tableName + " (`data`, `price`, `in_out`, `card`) VALUES" + " ('"
+					+ p_date + "', " + p_price + "', '" + p_in_out + "', '" + p_card + "')";
 
 			connection = DriverManager.getConnection(url, user, password);
 
 			statement = connection.createStatement();
 
-			resultValue = statement.executeUpdate(queryString);
-			
+			// resultValue =
+			statement.executeUpdate(queryString);
+
+			System.out.println("insert complete");
+
+		} catch (SQLException e) {
+			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
+		} finally {
+			closeDatabase();
+		}
+	}
+
+	public void productDelete(String p_date, int p_price, int p_in_out, int p_card) {//삭제
+		try {
+			String queryString = "DELETE FROM " + tableName + " (`data`, `price`, `in_out`, `card`) VALUES" + " ('"
+					+ p_date + "', " + p_price + "', '" + p_in_out + "', '" + p_card + "')";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			statement = connection.createStatement();
+
+			// resultValue =
+			statement.executeUpdate(queryString);
+
+			System.out.println("Delete complete");
+
 		} catch (SQLException e) {
 			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
 		} finally {
 			closeDatabase();
 		}
 
-		return resultValue;
-
 	}
-//	public int productDelete(String p_date,int p_price, int p_in_out, int p_card) {
-//		
-//	}
+	
+	
 }
+
--- SmartBudget/src/Database/Main.java
+++ SmartBudget/src/Database/Main.java
@@ -5,5 +5,6 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		JDBCExam jdbcExam = new JDBCExam();
+		jdbcExam.productInsert("2017-12-30", 9000, 1, 2);
 	}
 }
