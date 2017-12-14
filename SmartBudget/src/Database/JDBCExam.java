b3f475cca3a9d03c2a5f99fc717eeb781665c0a2
paki1019 <paki1019@gmail.com>
2017-12-14 18:25:17 +0900

    함수 추가

--- SmartBudget/src/Database/JDBCExam.java
+++ SmartBudget/src/Database/JDBCExam.java
@@ -82,31 +82,118 @@ public class JDBCExam {
 			closeDatabase();
 		}
 	}
-
-	public void productSelectOne(String date) {
+	
+	public void productUpdate(Data data1, Data data2) {
 		try {
-			String queryString = "SELECT * FROM budget WHERE date = '"+ date +"';";
-			
+			String queryString = "UPDATE budget set date = '" + data2.get_date() + "' , price = " + data2.get_price() + 
+					" , in_out = " + data2.get_in_out() + " , card = " + data2.get_card() + " , kinds = " + data2.get_kinds() + 
+					" , name = '" + data2.get_name() + "' where (date = '" + data1.get_date() + "' and price = " + data1.get_price() +
+					" and in_out = " + data1.get_in_out() + " and card = " + data1.get_card() + " and kinds = " + data1.get_kinds() + 
+					" and name = '" + data1.get_name() + "');";
 			connection = DriverManager.getConnection(url, user, password);
-			
+
 			statement = connection.createStatement();
-			
-			resultSet = statement.executeQuery(queryString);
-			
-			while (resultSet.next())
-			{
-				System.out.println(resultSet.getString("date") + "\t" + resultSet.getInt("price") +
-						"\t" + resultSet.getInt("in_out") +"\t"+  resultSet.getInt("card") +"\t"+
-						resultSet.getInt("kinds") +"\t"+resultSet.getString("name"));
-			}
-		}
-		catch (SQLException e)
-		{
+
+			// resultValue =
+			statement.executeUpdate(queryString);
+
+			System.out.println("update complete");	
+
+		} catch (SQLException e) {
 			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
-		}
-		finally
-		{
+		} finally {
 			closeDatabase();
+		}
+	}
+
+	public int productCountOne(String date) {
+		int re_count = 0;
+		try {
+			String queryString = "SELECT count(price) FROM budget WHERE date = '" + date + "';";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			statement = connection.createStatement();
+
+			resultSet = statement.executeQuery(queryString);
+			resultSet.next();
+			re_count = resultSet.getInt("count(price)");
+
+		} catch (SQLException e) {
+			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
+		} finally {
+			closeDatabase();
+			return re_count;
+		}
+	}
+
+	public Data productSelectOne(String date, int count) {// 해당 날짜의 데이터 객체를 반환
+		Data re_data = null;
+		try {
+			String queryString = "SELECT * FROM budget WHERE date = '" + date + "';";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			statement = connection.createStatement();
+
+			resultSet = statement.executeQuery(queryString);
+			for (int i = 0; i < count; i++)
+				resultSet.next();
+
+			re_data = new Data(resultSet.getString("date"), resultSet.getInt("price"), resultSet.getInt("in_out"),
+					resultSet.getInt("card"), resultSet.getInt("kinds"), resultSet.getString("name"));
+
+		} catch (SQLException e) {
+			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
+		} finally {
+			closeDatabase();
+			return re_data;
+		}
+	}
+
+	public int productSum(int year, int month, int i_o) {// 해달 월 합계 게산
+		int return_num = 0;
+		try {
+			String queryString = "SELECT SUM(price) FROM budget WHERE (date between '" + year + "-" + month
+					+ "-01' and '" + year + "-" + month + "-31') and (in_out=" + i_o + ");";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			statement = connection.createStatement();
+
+			resultSet = statement.executeQuery(queryString);
+
+			resultSet.next();
+
+			return_num = resultSet.getInt("sum(price)");
+		} catch (SQLException e) {
+			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
+		} finally {
+			closeDatabase();
+			return return_num;
+		}
+	}
+
+	public int productSum(int year, int month, int day, int i_o) {// 해당 일 합계 계산
+		int return_num = 0;
+		try {
+			String queryString = "SELECT SUM(price) FROM budget WHERE (date = '" + year + "-" + month + "-" + day
+					+ "') and (in_out=" + i_o + ");";
+
+			connection = DriverManager.getConnection(url, user, password);
+
+			statement = connection.createStatement();
+
+			resultSet = statement.executeQuery(queryString);
+
+			resultSet.next();
+
+			return_num = resultSet.getInt("sum(price)");
+		} catch (SQLException e) {
+			System.out.println("[쿼리 오류]\n" + e.getStackTrace());
+		} finally {
+			closeDatabase();
+			return return_num;
 		}
 	}
 }
--- SmartBudget/src/Database/Main.java
+++ SmartBudget/src/Database/Main.java
@@ -5,10 +5,13 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) {
 		JDBCExam jdbcExam = new JDBCExam();
-//		Data a = new Data(2017,12,8,80000,1,0,6,"gogi");
-//		Data b = new Data(2017,12,9,50000,1,1,2,"bab");
-//		Data c = new Data(2017,12,4,20000,1,0,6,"kimchi");
+		Data a, b;
 
-		jdbcExam.productSelectOne("2017-12-13");
+		a = jdbcExam.productSelectOne("2017-12-14", 1);
+		b = jdbcExam.productSelectOne("2017-12-04", 1);
+		jdbcExam.productUpdate(a, b);
+		// jdbcExam.productSelectOne("2017-12-13",2);
+		// Version ver = new Version();
+		// ver.versionupdate(0);
 	}
 }
