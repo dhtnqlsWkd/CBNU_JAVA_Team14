1b7d799a605baf662f9ee2fed1e16507d80f4f08
paki1019 <paki1019@gmail.com>
2017-12-15 00:05:07 +0900

    update

--- SmartBudget/src/Database/Main.java
+++ SmartBudget/src/Database/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		JDBCExam jdbcExam = new JDBCExam();
 		Data a, b;
-		jdbcExam.productUpdate(new Data("2017-12-6",8000,1,1,1,""),new Data("2017-12-6",6000,1,1,1,""));
+		jdbcExam.productInsert(new Data("2017-11-12",8000,1,1,1,""));
 //		a = jdbcExam.productSelectOne("2017-12-14", 1);
 //		b = jdbcExam.productSelectOne("2017-12-04", 1);
 //		jdbcExam.productUpdate(a, b);
